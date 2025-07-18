056bf284185a96dde2a7eb42166a0d037300c1ab
Marciah <marciah.machoka@student.moringaschool.com>
2025-07-18 09:25:16 +0300

    frontend

---
+++ spacer-frontend/src/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Make sure this exists
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
